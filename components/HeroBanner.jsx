331598754546506f98caaf15d78c481d6bb5b388
Illia Brytun <elbrytun@gmail.com>
2023-02-27 19:02:54 -0500

    add herobanner component

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React from 'react';
+import LINK from 'next/link';
 
-const HeroBanner = () => {
+import {urlFor} from '../lib/client';
+
+const HeroBanner = ({heroBanner}) => {
   return (
-    <div>HeroBanner</div>
+    <div className='hero-banner-container'>
+      <div>
+        <p className='beats-solo'>{heroBanner.smallText}</p>
+        <h3>{heroBanner.midText}</h3>
+        <h1>{heroBanner.largeText1}</h1>
+        <img src={urlFor(heroBanner.image)} alt="headphones" className='hero-banner-image' />
+        <div>
+          <LINK href={`/product/${heroBanner.product}`}>
+            <button type='button'>{heroBanner.buttonText}
+            </button>
+          </LINK>
+          <div className='desc'>
+            <h5>Description</h5>
+            <p>{heroBanner.desc}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
